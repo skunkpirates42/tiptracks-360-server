92be81907e3ba834711a33b6e65c6fe373bed80d
Peter Ramos <peterdramos02@gmail.com>
2019-02-04 11:18:40 -0500

    add setup for mongo and mongoose

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const passport = require('passport');
+const mongoose = require('mongoose');
 
 const { router: authRouter, localStrategy, jwtStrategy } = require('./auth/index');
 
-const { PORT } = require('./config');
+const { PORT, DATABASE_URL } = require('./config');
 
+// Create instance of express application
 const app = express();
 
 // Passport/Auth setup
 passport.use(localStrategy);
 passport.use(jwtStrategy);
 
-app.use('/api/auth/', authRouter);
-
+// Protect endpoints using JWT strategy
 const jwtAuth = passport.authenticate('jwt', { session: false });
+
+// Mount routers
+app.use('/api/auth/', authRouter);
 
 app.get('/api/protected', jwtAuth, (req, res) => {
   return res.json({
@@ -22,7 +26,21 @@ app.get('/api/protected', jwtAuth, (req, res) => {
   });
 });
 
+// Listen for incoming connections 
+if (require.main === module) {
+  // Connect to DB and listen for incoming connections
+  mongoose.connect(DATABASE_URL, { useNewUrlParser: true })
+    .then(instance => {
+      const conn = instance.connections[0];
+      console.info(`Connected to: monogodb://${conn.host}:${conn.port}/${conn.name}`);
+    })
+    .catch(err => {
+      console.error(err);
+    });
 
-app.listen(PORT , () => {
-  console.log(`Server is now listening on ${PORT}`);
-});
+  app.listen(PORT , function () {
+    console.info(`Server is now listening on port ${PORT}`);
+  }).on('error', err => {
+    console.error(err);
+  });
+}
