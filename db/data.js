3bda281c68380d0b3aa7aa04159665800aba8bcf
Peter Ramos <peterdramos02@gmail.com>
2019-02-09 16:11:12 -0500

    add more dummy data

--- db/data.js
+++ db/data.js
@@ -68,6 +68,76 @@ const tips = [
     totalTips: '120',
     userId: '000000000000000000000002'
   },
+  {
+    _id: '200000000000000000000006',
+    date: '2019-02-06',
+    baseWage: '5',
+    hours: '4',
+    notes: 'sdfsfs',
+    tippedOut: '5',
+    totalTips: '120',
+    userId: '000000000000000000000002'
+  },
+  {
+    _id: '200000000000000000000007',
+    date: '2019-02-05',
+    baseWage: '5',
+    hours: '5.5',
+    notes: 'sdfsfs',
+    tippedOut: '5',
+    totalTips: '120',
+    userId: '000000000000000000000001'
+  },
+  {
+    _id: '200000000000000000000008',
+    date: '2019-02-07',
+    baseWage: '5',
+    hours: '4',
+    notes: 'sdfsfs',
+    tippedOut: '5',
+    totalTips: '120',
+    userId: '000000000000000000000002'
+  },
+  {
+    _id: '200000000000000000000009',
+    date: '2019-02-06',
+    baseWage: '5',
+    hours: '4.5',
+    notes: 'sdfsfs',
+    tippedOut: '5',
+    totalTips: '120',
+    userId: '000000000000000000000001'
+  },
+  {
+    _id: '200000000000000000000010',
+    date: '2019-02-08',
+    baseWage: '5',
+    hours: '7',
+    notes: 'sdfsfs',
+    tippedOut: '5',
+    totalTips: '100',
+    userId: '000000000000000000000002'
+  },
+  {
+    _id: '200000000000000000000011',
+    date: '2019-02-07',
+    baseWage: '5',
+    hours: '4.25',
+    notes: 'sdfsfs',
+    tippedOut: '5',
+    totalTips: '110',
+    userId: '000000000000000000000001'
+  },
+  {
+    _id: '200000000000000000000012',
+    date: '2019-02-09',
+    baseWage: '5',
+    hours: '6.5',
+    notes: 'sdfsfs',
+    tippedOut: '5',
+    totalTips: '130',
+    userId: '000000000000000000000002'
+  },
 ];
 
 module.exports = { users, tips };
